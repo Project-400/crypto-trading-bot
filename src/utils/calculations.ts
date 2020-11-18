72fb31e660da2d6dc8b893cc9930e1a58087760b
Charles McCarthy <charles@studybundles.com>
2020-11-18 19:57:26 +0000

    Precision Rounding Of Percentage & Price Calculations

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -1,13 +1,16 @@
 export default class Calculations {
 
-	public static PriceDifference = (startPrice: number, endPrice: number): number => startPrice - endPrice;
+	public static PriceDifference = (startPrice: number, endPrice: number, precision: number): number =>
+		Number((endPrice - startPrice).toFixed(precision))
 
 	public static PricePercentageDifference = (startPrice: number, endPrice: number): number => { // Rounded to prevent small number issues
 		const roundedStartPrice: number = startPrice * 1000;
 		const roundedEndPrice: number = endPrice * 1000;
 		const roundedPriceDifference: number = roundedEndPrice - roundedStartPrice;
 
-		return (roundedEndPrice !== roundedStartPrice) ? ((roundedPriceDifference / roundedStartPrice) * 100) : 0;
+		return (roundedEndPrice !== roundedStartPrice) ?
+			Number(((roundedPriceDifference / roundedStartPrice) * 100).toFixed(4)) :
+			0;
 	}
 
 }
