1f3681970e07ffbda17940a90637d8e4e25facaa
Charles McCarthy <charles@studybundles.com>
2020-11-20 22:35:12 +0000

    Capitalise Env Value Names

--- src/environment.ts
+++ src/environment.ts
@@ -4,5 +4,6 @@ dotenv.config();
 
 export const BinanceWS: string = process.env.BINANCE_WS as string; // Deprecated
 export const BINANCE_WS: string = process.env.BINANCE_WS as string;
-export const BinanceAPI: string = process.env.BINANCE_API as string;
-export const CrudServiceUrl: string = process.env.CRUD_SERVICE_URL as string;
+export const BinanceAPI: string = process.env.BINANCE_API as string; // Deprecated
+export const BINANCE_API: string = process.env.BINANCE_API as string;
+export const CRUD_SERVICE_URL: string = process.env.CRUD_SERVICE_URL as string;
