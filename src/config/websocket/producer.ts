b732e13ded1426486411be800af1c169776cc6bb
Charles McCarthy <charles@studybundles.com>
2021-01-27 13:26:38 +0000

    Assign IDs To Connected Socket Clients

--- src/config/websocket/producer.ts
+++ src/config/websocket/producer.ts
@@ -3,13 +3,14 @@ import * as WebSocket from 'ws';
 // tslint:disable-next-line:no-duplicate-imports
 import { AddressInfo } from 'ws';
 import express from 'express';
+import { v4 as uuid } from 'uuid';
 
 export class WebsocketProducer {
 
 	private static server: http.Server;
 	private static wss: WebSocket.Server;
 	// private static webS: WebSocket;
-	private static connectedSockets: WebSocket[] = [];
+	private static connectedSockets: WebSocket[] = []; // List of connected clients
 
 	public static setup = (app: express.Application): void => {
 		WebsocketProducer.server = http.createServer(app);
@@ -20,7 +21,11 @@ export class WebsocketProducer {
 
 		WebsocketProducer.wss.on('connection', (ws: WebSocket): void => {
 			// WebsocketProducer.webS = ws;
+			// tslint:disable-next-line:ban-ts-ignore
+			// @ts-ignore
+			ws.id = uuid();
 			WebsocketProducer.connectedSockets.push(ws);
+			console.log(WebsocketProducer.connectedSockets);
 
 			ws.on('message', (message: string): void => {
 				console.log('received: %s', message);
@@ -31,7 +36,24 @@ export class WebsocketProducer {
 		});
 
 		WebsocketProducer.wss.on('close', (ws: WebSocket): void => {
+			console.log(WebsocketProducer.connectedSockets);
 			console.log('Connection closed');
+		});
+
+		WebsocketProducer.wss.on('unexpected-response', (ws: WebSocket): void => {
+			console.log('Unexpected Response in Websocket Producer');
+		});
+
+		WebsocketProducer.wss.on('ping', (ws: WebSocket): void => {
+			console.log('Websocket Producer Ping');
+		});
+
+		WebsocketProducer.wss.on('pong', (ws: WebSocket): void => {
+			console.log('Websocket Producer Pong');
+		});
+
+		WebsocketProducer.wss.on('error', (ws: WebSocket): void => {
+			console.log('Websocket Producer Error');
 		});
 
 		WebsocketProducer.server.listen(process.env.PORT || 8999, (): void => {
@@ -45,7 +67,17 @@ export class WebsocketProducer {
 	public static sendMessage = (msg: string): void => {
 		try {
 			// WebsocketProducer.webS.send(msg);
-			WebsocketProducer.connectedSockets.map((socket: WebSocket): void => socket.send(msg));
+			WebsocketProducer.connectedSockets.map((socket: WebSocket): void => {
+				// tslint:disable-next-line:ban-ts-ignore
+				// @ts-ignore
+				if (socket.CLOSED) console.log(`Connection ${socket.id} is closed`);
+				if (socket.OPEN) {
+					// tslint:disable-next-line:ban-ts-ignore
+					// @ts-ignore
+					console.log(`Connection ${socket.id} is open`);
+					socket.send(msg);
+				}
+			});
 		} catch (e) {
 			console.error(`The error likely occurred because there are no clients subscribed to the Websocket. Error: ${e.message}`);
 		}
