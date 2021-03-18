e48e51055fb7bd4a978bf20852c10fec50f1d384
Charles McCarthy <charles@studybundles.com>
2021-03-18 11:28:30 +0000

    Generate New SQS Queue On App Startup

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes';
 import { WebsocketProducer } from './config/websocket/producer';
+import { SQSConsumer } from './sns-sqs/consumer';
+import * as AWS from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
 
 const app: express.Application = express();
 
@@ -13,10 +16,25 @@ app.use(cookieParser());
 // app.use(session({ secret: 'test-secret' }));
 app.use('/v1', indexRouter);
 
+console.log('process.env.IS_LOCAL');
+console.log(process.env.IS_LOCAL);
+
+if (process.env.IS_LOCAL) {
+	const instanceId: string = uuid();
+	SQSConsumer.SetupConsumer(instanceId);
+} else {
+	const meta: any = new AWS.MetadataService();
+
+	meta.request('/latest/meta-data/instance-id', (err: Error, instanceId: string): void => {
+		console.log(`Instance Id: ${instanceId}`);
+		SQSConsumer.SetupConsumer(instanceId);
+	});
+}
+
 WebsocketProducer.setup(app);
 
 app.listen(3001, '0.0.0.0', (): void => {
-	console.log('Listening to port:  ' + 3000);
+	console.log('Listening to port: ' + 3000);
 });
 
 export default app;
--- src/sns-sqs/consumer.ts
+++ src/sns-sqs/consumer.ts
@@ -10,9 +10,17 @@ AWS.config.update({
 
 export class SQSConsumer {
 
-	public static SetupConsumer = (): void => {
+	public static SetupConsumer = async (instanceId: string): Promise<void> => {
+		const queueName: string = await SQSConsumer.CreateQueue(instanceId);
+		SQSConsumer.ListenToQueue(queueName);
+		setTimeout(async (): Promise<void> => {
+			await SQSConsumer.SubscribeToSNSTopic(queueName);
+		}, 3000);
+	}
+
+	public static ListenToQueue = (queueName: string): void => {
 		const consumer: Consumer = Consumer.create({
-			queueUrl: `https://sqs.eu-west-1.amazonaws.com/068475715603/BotQueue2`,
+			queueUrl: `https://sqs.eu-west-1.amazonaws.com/068475715603/${queueName}`,
 			handleMessage: async (message: SQSMessage): Promise<void> => {
 				await SQSConsumer.HandleMessage(message);
 			},
@@ -38,6 +46,57 @@ export class SQSConsumer {
 		});
 
 		consumer.start();
+	}
+
+	private static SubscribeToSNSTopic = (queueName: string): Promise<boolean> => {
+		const sns: AWS.SNS = new AWS.SNS();
+
+		const topicArn: string = 'arn:aws:sns:eu-west-1:068475715603:TestTopic';
+		const sqsEndpoint: string = `arn:aws:sqs:eu-west-1:068475715603:${queueName}`;
+
+		const params: AWS.SNS.SubscribeInput = {
+			Protocol: 'sqs',
+			TopicArn: topicArn,
+			Endpoint: sqsEndpoint
+		};
+
+		return new Promise((resolve: any, reject: any): void => {
+			sns.subscribe(params, (err: AWS.AWSError, data: AWS.SNS.SubscribeResponse): void => {
+				if (err) {
+					console.log(`Failed to subscribe SQS queue ${sqsEndpoint} to SNS Topic ${topicArn}`);
+					console.log(err, err.stack);
+					reject(false);
+				} else {
+					console.log(`Successfully subscribed to SNS Topic`);
+					resolve(true);
+				}
+			});
+		});
+	}
+
+	private static CreateQueue = (instanceId: string): Promise<string> => {
+		const sqs: AWS.SQS = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+		const queueName: string = `crypto-bot-${instanceId}`;
+
+		const params: AWS.SQS.CreateQueueRequest = {
+			QueueName: queueName,
+			Attributes: {
+				MessageRetentionPeriod: '86400'
+			}
+		};
+
+		return new Promise((resolve: any, reject: any): void => {
+			sqs.createQueue(params, (err: AWS.AWSError, data: AWS.SQS.CreateQueueResult): void => {
+				if (err) {
+					console.error(`Error Creating SQS Queue ${queueName}`, err);
+					reject(false);
+				} else {
+					console.log(`Successfully Created SQS Queue ${queueName}`);
+					resolve(queueName);
+				}
+			});
+		});
 	}
 
 	private static HandleMessage = (message: SQSMessage): void => {
