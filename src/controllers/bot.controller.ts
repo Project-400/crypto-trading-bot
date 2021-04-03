2b05b89401853da147122aaf3bbff125952d6c4e
Charles McCarthy <charles@studybundles.com>
2021-04-03 15:27:10 +0100

    Test Controller

--- src/controllers/bot.controller.ts
+++ src/controllers/bot.controller.ts
@@ -8,53 +8,6 @@ import { BotTradeData } from '../models/bot-trade-data';
 
 export class BotController {
 
-	public static deployBot = async (req: Request, res: Response): Promise<Response> => {
-		// if (!req.body || !req.body.botId) return res.status(400).json({ error: 'Invalid request body' });
-		// if (!req.body) return res.status(400).json({ error: 'Invalid request body' });
-
-		const botId: string = req.body.botId.toString();
-		let bot: ShortTermTraderBot | undefined;
-
-		try {
-			bot = await BotManager.deployNewBot('GTOBTC', 0.0001, false);
-			console.log(2);
-			console.log(bot);
-		} catch (e) {
-			console.log('Bot failed to deploy');
-			console.log(e);
-
-			if (bot) await BotManager.shutdownBot(botId);
-			return res.status(400).json({ success: false });
-		}
-		return res.status(200).json({ success: true, message: 'Started Bot', bot });
-	}
-
-	public static stopBot = async (req: Request, res: Response): Promise<Response> => {
-		if (!req.body || !req.body.botId) return res.status(400).json({ error: 'Invalid request body' });
-
-		const botId: string = req.body.botId.toString();
-		const bot: ShortTermTraderBot | undefined = BotManager.getBot(botId);
-
-		if (!bot) return res.status(404).json({ error: 'Bot not found' });
-
-		await BotManager.shutdownBot(botId);
-
-		return res.status(200).json({ success: true, message: 'Stopped Bot', bot });
-	}
-
-	public static pauseBot = (req: Request, res: Response): Response => {
-		if (!req.body || !req.body.botId) return res.status(400).json({ error: 'Invalid request body' });
-
-		const botId: string = req.body.botId.toString();
-		const bot: ShortTermTraderBot | undefined = BotManager.getBot(botId);
-
-		if (!bot) return res.status(404).json({ error: 'Bot not found' });
-
-		BotManager.pauseBot(botId);
-
-		return res.status(200).json({ success: true, message: 'Paused Bot', bot });
-	}
-
 	public static getBot = (req: Request, res: Response): Response => {
 		if (!req.query || !req.query.botId) return res.status(400).json({ error: 'Invalid request parameters' });
 
@@ -64,18 +17,6 @@ export class BotController {
 		if (!bot) return res.status(404).json({ error: 'Bot not found' });
 
 		return res.status(200).json({ success: true, bot });
-	}
-
-	public static getAllBots = (req: Request, res: Response): Response => {
-		const bots: ShortTermTraderBot[] = BotManager.getAllBots();
-
-		return res.status(200).json({ success: true, bots });
-	}
-
-	public static getBotCount = (req: Request, res: Response): Response => {
-		const botCount: number = BotManager.getBotCount();
-
-		return res.status(200).json({ success: true, botCount });
 	}
 
 	public static shutdownBots = (req: Request, res: Response): Response => {
@@ -130,7 +71,12 @@ export class BotController {
 		if (!req.body || !req.query.botId) return res.status(400).json({ error: 'Invalid request params' });
 		const botId: string = req.query.botId.toString();
 
-		await BotManager.shutdownBot(botId);
+		try {
+			const shutdown: boolean = await BotManager.shutdownBot(botId);
+			if (!shutdown) return res.status(404).json({ success: false });
+		} catch (e) {
+			return res.status(500).json({ success: false });
+		}
 
 		return res.status(200).json({ success: true });
 	}
---
+++ src/controllers/test.controller.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express';
+import ShortTermTraderBot from '../bots/short-term-trader-bot';
+import { BotManager } from '../bots/bot-manager';
+import { BotTradeData } from '../models/bot-trade-data';
+
+export class TestController {
+
+	public static pauseBot = (req: Request, res: Response): Response => {
+		if (!req.body || !req.body.botId) return res.status(400).json({ error: 'Invalid request body' });
+
+		const botId: string = req.body.botId.toString();
+		const bot: ShortTermTraderBot | undefined = BotManager.getBot(botId);
+
+		if (!bot) return res.status(404).json({ error: 'Bot not found' });
+
+		BotManager.pauseBot(botId);
+
+		return res.status(200).json({ success: true, message: 'Paused Bot', bot });
+	}
+
+	public static getAllBots = (req: Request, res: Response): Response => {
+		const bots: ShortTermTraderBot[] = BotManager.getAllBots();
+
+		return res.status(200).json({ success: true, bots });
+	}
+
+	public static getBotCount = (req: Request, res: Response): Response => {
+		const botCount: number = BotManager.getBotCount();
+
+		return res.status(200).json({ success: true, botCount });
+	}
+
+	public static getBotTradeData = async (req: Request, res: Response): Promise<Response> => {
+		if (!req.body || !req.query.botId) return res.status(400).json({ error: 'Invalid request params' });
+		const botId: string = req.query.botId.toString();
+
+		const bot: ShortTermTraderBot | undefined = BotManager.getBot(botId);
+
+		if (!bot) return res.status(404).json({ error: 'Bot not found' });
+		const tradeData: BotTradeData[] = bot.getAllTradeData();
+
+		return res.status(200).json({ success: true, tradeData });
+	}
+
+	public static getBot = (req: Request, res: Response): Response => {
+		if (!req.query || !req.query.botId) return res.status(400).json({ error: 'Invalid request parameters' });
+
+		const botId: string = req.query.botId.toString();
+		const bot: ShortTermTraderBot | undefined = BotManager.getBot(botId);
+
+		if (!bot) return res.status(404).json({ error: 'Bot not found' });
+
+		return res.status(200).json({ success: true, bot });
+	}
+
+	public static shutdownBots = (req: Request, res: Response): Response => {
+		const count: number = BotManager.shutdownAllBots();
+
+		return res.status(200).json({ success: true, message: 'Shutdown all bots', count });
+	}
+
+}
