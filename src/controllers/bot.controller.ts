8f7700d634e213ae425b4b0a1d5471c2ac9938ad
Charles McCarthy <charles@studybundles.com>
2021-04-19 19:06:21 +0100

    Managed Unassigned Currency Bots

--- src/controllers/bot.controller.ts
+++ src/controllers/bot.controller.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from 'express';
 import { BotConductor } from '../bots/bot-conductor';
 import ShortTermTraderBot from '../bots/short-term-trader-bot';
+import { BotWaitingQueue } from '../services/bot-waiting-queue';
 
 export class BotController {
 
 	public static createBot = async (req: Request, res: Response): Promise<Response> => {
 		// || !req.query.clientSocketId
-		if (!req.body || !req.body.botId || !req.body.currency || !req.body.quoteAmount || req.body.repeatedlyTrade === undefined)
+		if (!req.body || !req.body.botId || !req.body.quoteAmount || req.body.repeatedlyTrade === undefined || req.body.currencyPreChosen === undefined)
 			return res.status(400).json({ error: 'Invalid request params' });
 		//
 		const botId: string = req.body.botId.toString();
-		const currency: string = req.body.currency.toString();
+		const currencyPreChosen: boolean = req.body.currencyPreChosen.toString() === 'true';
+		const currency: string = currencyPreChosen ? req.body.currency.toString() : undefined;
+
 		const quoteAmount: number = parseFloat(req.body.quoteAmount.toString());
 		const repeatedlyTrade: boolean = req.body.repeatedlyTrade.toString() === 'true';
 		const percentageLoss: number = req.query.percentageLoss ? parseFloat(req.query.percentageLoss.toString()) : 1;
@@ -19,12 +22,22 @@ export class BotController {
 		// const priceInfo: GetSymbolPriceTickerDto = await BinanceApi.getCurrentPrice(currency);
 		// const bot: ShortTermTraderBot | undefined =
 		// 	await BotConductor.deployNewBot(currency, quoteAmount, repeatedlyTrade, clientSocketId, percentageLoss);
-		const bot: ShortTermTraderBot | undefined =
-			await BotConductor.deployNewBot(botId, currency, quoteAmount, repeatedlyTrade, percentageLoss);
 
-		if (!bot) return res.status(500).json({ success: false });
+		if (currencyPreChosen) {
+			const bot: ShortTermTraderBot | undefined =
+				await BotConductor.deployNewBot(botId, currency, quoteAmount, repeatedlyTrade, percentageLoss);
 
-		return res.status(200).json({ success: true, bot: bot?.BOT_DETAILS() });
+			if (!bot) return res.status(500).json({ success: false });
+		} else {
+			BotWaitingQueue.AddBot({
+				botId,
+				quoteAmount,
+				repeatedlyTrade,
+				percentageLoss
+			});
+		}
+
+		return res.status(200).json({ success: true });
 	}
 
 	public static stopBot = async (req: Request, res: Response): Promise<Response> => {
